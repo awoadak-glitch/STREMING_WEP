4e18d7d9ce4a6a36c4e94b2d685fbbe8525379be
awoadak-glitch <awoadak@gmail.com>
2026-07-08 21:06:49 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // تفعيل static export إذا كنت سترفعه على GitHub Pages فقط
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  images: { unoptimized: true },
 };
-
 export default nextConfig;
